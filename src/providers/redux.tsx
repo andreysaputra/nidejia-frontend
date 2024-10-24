fb59a1b417e3d10fff86fb723301deb0b3403a4d
andreysaputra <sptrandrey@gmail.com>
2024-10-24 11:58:54 +0700

    fist commit

---
+++ src/providers/redux.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { store } from "@/lib/store";
+import { Provider } from "react-redux";
+import { SessionProvider } from "next-auth/react";
+
+export default function ReduxProvider({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <SessionProvider>
+      <Provider store={store}>{children}</Provider>
+    </SessionProvider>
+  );
+}
